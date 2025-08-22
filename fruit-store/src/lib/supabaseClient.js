e83b0269a4ba13723fc63c417762c62cfece8454
JOSEPH-KIBIRU <josephkibiru19@gmail.com>
2025-08-22 21:27:59 +0300

    Update supabaseClient.js

--- fruit-store/src/lib/supabaseClient.js
+++ fruit-store/src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL="https://elcnsueulfcyiuskjarf.supabase.co"
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsY25zdWV1bGZjeWl1c2tqYXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzkzNjMsImV4cCI6MjA3MTExNTM2M30.VrHlFH64BkDojmJZNMcfeZhPG164s3pqcVtYGwf136Y"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
